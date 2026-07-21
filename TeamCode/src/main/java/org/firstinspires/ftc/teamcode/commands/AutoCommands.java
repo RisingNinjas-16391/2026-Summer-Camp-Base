e538d4beaa63f86e64dbb77646dc3bf51315f14f
Team-2383 <pl248882@ahschool.com>
2026-07-21 09:03:16 -0400

    final commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/AutoCommands.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/AutoCommands.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.commands;
 import com.acmerobotics.dashboard.config.Config;
 
 import org.firstinspires.ftc.teamcode.subsystems.Subsystems;
+import org.firstinspires.ftc.teamcode.subsystems.pivot.Pivot;
+import org.firstinspires.ftc.teamcode.subsystems.pivot.PivotConstants;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -50,7 +52,19 @@ public class AutoCommands {
 
     public static Command blueAuto(Subsystems subsystems) {
         return Commands.sequence(
-                DriveCommands.forward(subsystems.drive(), 10)
+                DriveCommands.forward(subsystems.drive(), 10),
+                DriveCommands.turn(subsystems.drive(), 90),
+                DriveCommands.forward(subsystems.drive(), 10),
+                DriveCommands.turn(subsystems.drive(), 90),
+                DriveCommands.forward(subsystems.drive(), 10),
+                DriveCommands.turn(subsystems.drive(), 90),
+                DriveCommands.forward(subsystems.drive(), 10),
+                DriveCommands.turn(subsystems.drive(), 90),
+
+
+                Pivot.setPosition(subsystems.pivot(), PivotConstants.HIGH)
+
+
         );
     }
 
